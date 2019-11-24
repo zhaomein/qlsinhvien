985a0e3b4ac8896417000096f5d9092b3e05386f
unknown <trieuniemit@gmail.com>
2019-11-24 16:42:13 +0700

    update router

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 Route::post('register', 'Api\AuthController@register');
 Route::post('login', 'Api\AuthController@login');
 Route::group(['middleware' => 'jwt.auth'], function () {
-    Route::post('logout', 'Api\AuthController@logout');
+    Route::get('logout', 'Api\AuthController@logout');
 
     Route::resource('students', 'Api\StudentController');
 
